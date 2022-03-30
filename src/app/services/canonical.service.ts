4764af2e6d85fdc0cfdd3f97c5ff8eba57fea6e8
keval101 <kevalvadhiya101@gmail.com>
2022-03-30 23:36:30 +0530

    add tags

--- src/app/services/canonical.service.ts
+++ src/app/services/canonical.service.ts
@@ -24,23 +24,19 @@ export class CanonicalService {
     this._meta.addTags([
       { name: "description", content: tagsDesctiption.description},
       { name: 'keywords', content: tagsDesctiption.keywords },
+
+      { property: 'og:type', content: 'website'},
+      { property: 'og:url', content: `${this.pageUrl}`},
+      { property: 'og:title', content: tagsDesctiption.metaTitle},
+      { property: 'og:description', content: tagsDesctiption.description},
+      { property: 'og:image', content: `https://bhagavad-gita.netlify.app/assets/krishnaArjunaa.jpg`},
+
+      { name: 'twitter:site', content: `@BhagavadGita`},
+      { name: 'twitter:title', content: tagsDesctiption.metaTitle},
+      { name: 'twitter:description', content: tagsDesctiption.description},
+      { name: 'twitter:image', content: `https://bhagavad-gita.netlify.app/assets/krishnaArjunaa.jpg`},
+      { name: 'twitter:card', content: 'summary_large_image'},
     ])
-    this._meta.updateTag( { name: "description", content: tagsDesctiption.description }, "name=description");
-    this._meta.updateTag( { name: "keywords", content: tagsDesctiption.keywords }, "name=keywords");
-
-    this._meta.updateTag( {name:'twitter:card', content: 'summary_large_image'}, "name=twitter:card");
-    this._meta.updateTag( {name:'twitter:site', content: '@BhagavadGita'}, "name=twitter:site");
-    this._meta.updateTag( {name:'twitter:creator', content: '@KevalVadhiya'}, "name=twitter:creator");
-    this._meta.updateTag( {name:'twitter:title', content: tagsDesctiption.metaTitle}, "name=twitter:title");
-    this._meta.updateTag( {name:'twitter:description', content: tagsDesctiption.description}, "name=twitter:description");
-    this._meta.updateTag( {name:'twitter:image', content: 'https://bhagavad-gita.netlify.app/assets/krishnaArjunaa.jpg'}, "name=twitter:image");
-
-    this._meta.updateTag( {property:'og:type', content: 'website'}, "property=og:type");
-    this._meta.updateTag( {property:'og:url', content: `${this.pageUrl}`}, "property=og:url");
-    this._meta.updateTag( {property:'og:title', content: tagsDesctiption.metaTitle}, "property=og:title");
-    this._meta.updateTag( {property:'og:description', content: tagsDesctiption.description}, "property=og:description");
-    this._meta.updateTag( {property:'og:image', content: 'https://bhagavad-gita.netlify.app/assets/krishnaArjunaa.jpg'}, "property=og:image");
-
   }
 
   updateMetaTags(description, keywords): void {
