b102b4766828db6f6edeb1eea0f2cf853494b2c8
Keval <kevalvadhiya101@gmail.com>
2024-03-29 18:05:53 +0530

    Fix SSR

--- server.ts
+++ server.ts
@@ -1,55 +1,37 @@
-import 'zone.js/dist/zone-node';
+import 'zone.js/node';
 
+import { APP_BASE_HREF } from '@angular/common';
 import { ngExpressEngine } from '@nguniversal/express-engine';
 import * as express from 'express';
-import { join } from 'path';
-
-import { AppServerModule } from './src/main.server';
-import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
+import { join } from 'path';
+import { AppServerModule } from './src/main.server';
 
-const MockBrowser = require('mock-browser').mocks.MockBrowser;
-const mock = new MockBrowser();
-
-global['navigator'] = mock.getNavigator();
-const cache = require('memory-cache');
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
-  const distFolder = join(process.cwd(), 'dist/BhagvadGita/browser');
+  const distFolder = join(process.cwd(), 'dist/blog/browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
-  // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
+  // Our Universal express-engine (found @ https://github.com/angular/universal/tree/main/modules/express-engine)
   server.engine('html', ngExpressEngine({
-    bootstrap: AppServerModule,
+    bootstrap: AppServerModule
   }));
 
   server.set('view engine', 'html');
   server.set('views', distFolder);
 
-  
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
   server.get('*.*', express.static(distFolder, {
-    maxAge: '1y',
-    redirect: false
+    maxAge: '1y'
   }));
 
   // All regular routes use the Universal engine
-  server.get('*', (req: any, res: any) => {
-    const entry = cache.get(req.originalUrl); // check if we have a cache entry
-    if (entry) {
-      res.send(entry);                        // send the cache entry
-    } else {
-      res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] }, (err:any, html:any) => {
-        cache.put(req.originalUrl, html, 20000);     // save the HTML in the cache
-        res.send(html);
-      });
-    }
-    
+  server.get('*', (req, res) => {
+    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
   });
-
 
   return server;
 }
